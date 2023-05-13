d21048ca6427cc2739b3a3b96909dcd4500f740a
Muhammad Hamza Shaikh <hamzashaikhwork@gmail.com>
2023-05-14 01:45:37 +0500

    feat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import UserLayout from "layouts/user";
 import AuthLayout from "layouts/auth";
+import Signin from "views/account/Signin";
+import Signup from "views/account/Signup";
+
 const App = () => {
   return (
     <Routes>
@@ -13,6 +15,8 @@ const App = () => {
       <Route path="user/*" element={<UserLayout />} />
       <Route path="rtl/*" element={<RtlLayout />} />
       <Route path="/" element={<Navigate to="/admin" replace />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
