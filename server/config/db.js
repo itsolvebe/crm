f2eb0251cce3901e5b613924549dfe7e6db752a6
Mohammad Riyan <mohammadriyan428@gmail.com>
2023-05-15 17:02:26 +0500

    feat - Initialize server and integrate server-side authentication

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+// database connection file to MongoDB
+const { mongoose } = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(`mongodb connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.underline.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server/model/User.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   role: {
+//     type: String,
+//     enum: ["Admin", "User", "Ticket Manager", "Employee"],
+//     default: "User",
+//   },
+// });
+
+// const User = mongoose.model("User", userSchema);
+
+// module.exports = User;
+
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// hash user's password with salt before saving document to db
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// extend matchPassword function unto userSchema
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
